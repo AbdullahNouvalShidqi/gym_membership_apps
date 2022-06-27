f98fa642d444a1fb6536967522422200efea7ee7
AbdullahNouvalShidqim <abdullah.nouval@gmail.com>
2022-06-27 20:43:20 +0700

    delete something

--- lib/screen/available_class/available_class_view_model.dart
+++ lib/screen/available_class/available_class_view_model.dart
@@ -25,22 +25,6 @@ class AvailableClassViewModel with ChangeNotifier {
     try {
       _currentItems = item;
       _availableClasses = await MainAPI().getAllClass(type: item.type);
-      // [
-      //   ClassModel(
-      //     id: 5,
-      //     name: 'Zumba',
-      //     description: 'Cardio is defined as any type of exercise that gets your heart rate up and keeps it up for a prolonged period of time.',
-      //     startAt: DateTime.now(),
-      //     endAt: DateTime.now().add(const Duration(hours: 3)),
-      //     qtyUsers: 25,
-      //     type: 'Online',
-      //     images: ['cardio.png'],
-      //     instructor: InstructorModel(id: 4, name: 'Aldi Amal', contact: '087823232237', email: 'aldiamal@gmail.com'),
-      //     price: 300000,
-      //     location: 'Room A'
-      //   ),
-      // ];
-      // print(DateTime.now());
       _availableClasses = _availableClasses.where((element) => element.name == item.name).toList();
       for (var i = 0; i < _availableClasses.length; i++) {
         _availableClasses[i].images = [...item.images];
